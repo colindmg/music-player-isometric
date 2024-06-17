2c19ee32c1b5f4c58fde3a0e5c54fc2821bfcd3a
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-17 22:21:59 +0200

    Animation de l'overlay pour transition entre les deux vues

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Scene from "./components/Scene";
 function App() {
   const [currentAlbumData, setCurrentAlbumData] = useState(null);
   const [hoveredAlbumData, setHoveredAlbumData] = useState(null);
+  const [displayOverlay, setDisplayOverlay] = useState(false);
 
   return (
     <>
@@ -44,14 +45,28 @@ function App() {
         {currentAlbumData && (
           <button
             onClick={() => {
-              setCurrentAlbumData(null);
+              setDisplayOverlay(true);
+              setTimeout(() => {
+                setCurrentAlbumData(null);
+              }, 500);
+              setTimeout(() => {
+                setDisplayOverlay(false);
+              }, 800);
               window.scrollTo(0, 0);
             }}
-            className="absolute left-20 top-1/2 p-2 rounded-full bg-neutral-900 "
+            className="absolute left-20 top-1/2 p-2 rounded-full bg-neutral-900 hover:scale-105 transition-transform duration-200"
           >
             <img src="/img/icons/return-arrow.svg" className="w-4 h-4" />
           </button>
         )}
+
+        {/* OVERLAY DE TRANSITION ENTRE LES DEUX VUES */}
+        <div
+          className="pointer-events-none fixed top-0 left-0 bg-gray-50 w-screen h-screen z-50 transition-opacity duration-500"
+          style={{
+            opacity: displayOverlay ? 1 : 0,
+          }}
+        ></div>
 
         {/* LOGO */}
         <img
