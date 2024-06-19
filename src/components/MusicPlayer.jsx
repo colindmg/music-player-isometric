f061e140fbd016c9419c7bd99ed076db1c20b62b
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-19 17:42:15 +0200

    Barre de progression du morceau jouée fonctionnelleé

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,9 +1,44 @@
 import PropTypes from "prop-types";
+import { useEffect, useRef, useState } from "react";
 
 const MusicPlayer = ({ isHidden, playingAlbum, playingSongIndex }) => {
+  const audioRef = useRef(null);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      audioElement.addEventListener("loadedmetadata", () => {
+        setDuration(audioElement.duration);
+      });
+
+      audioElement.addEventListener("timeupdate", () => {
+        setCurrentTime(audioElement.currentTime);
+      });
+    }
+
+    return () => {
+      if (audioElement) {
+        audioElement.removeEventListener("loadedmetadata", () => {
+          setDuration(audioElement.duration);
+        });
+
+        audioElement.removeEventListener("timeupdate", () => {
+          setCurrentTime(audioElement.currentTime);
+        });
+      }
+    };
+  }, [playingAlbum, playingSongIndex]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
   return (
     <div
-      className="absolute w-96 top-36 left-10 bg-[#F1F1F1] p-2.5 rounded-lg"
+      className="absolute w-[350px] top-36 left-10 bg-[#F1F1F1] p-2.5 rounded-lg transition-opacity duration-300"
       style={{ opacity: isHidden ? 0 : 100 }}
     >
       {/* INFORMATIONS SUR LE SON EN TRAIN DE JOUER */}
@@ -39,7 +74,20 @@ const MusicPlayer = ({ isHidden, playingAlbum, playingSongIndex }) => {
         )}
       </div>
 
-      {/* <audio
+      {/* BARRE DE LECTURE DU MORCEAU */}
+      <div className="flex items-center gap-2 mt-3 mr-1">
+        <p className="text-[#737373] text-xs w-8">{formatTime(currentTime)}</p>
+        <div className="relative w-full h-1 bg-white rounded-full">
+          <div
+            className="absolute h-full bg-neutral-900 rounded-full"
+            style={{ width: `${(currentTime / duration) * 100}%` }}
+          ></div>
+        </div>
+        <p className="text-[#737373] text-xs">{formatTime(duration)}</p>
+      </div>
+
+      <audio
+        ref={audioRef}
         src={
           playingAlbum?.songs[playingSongIndex].mp3
             ? playingAlbum?.songs[playingSongIndex].mp3
@@ -47,7 +95,7 @@ const MusicPlayer = ({ isHidden, playingAlbum, playingSongIndex }) => {
         }
         autoPlay
         preload="auto"
-      ></audio> */}
+      ></audio>
     </div>
   );
 };
